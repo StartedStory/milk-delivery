0382f713d0c5b339c80c21fe1df6ddff4f78be2f
marikgrabowski2000 <topdev39@gmail.com>
2022-01-16 23:10:27 +0300

    feat: add page footer

--- truffle-config.js
+++ truffle-config.js
@@ -18,12 +18,12 @@ module.exports = {
 
     rinkeby: {
       provider: () => new HDWalletProvider(MNEMONIC, `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_APP_URL}`),
-    network_id: 4,
-    gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets )
-    networkCheckTimeout: 1000000000
+      network_id: 4,
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets )
+      networkCheckTimeout: 1000000000
     },
   },
 
