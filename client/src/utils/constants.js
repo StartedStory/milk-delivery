1b294afcf82ac9c2a4a99fd8ed677b7d6dbc3cf2
marikgrabowski2000 <topdev39@gmail.com>
2022-04-12 01:16:54 +0300

    feat: modify contract

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -2,6 +2,6 @@ import abi from './artifacts/MilkDelivery.json';
 import rinkebyAbi from './artifacts/MilkDeliveryOnRinkeby.json';
 
 export const LocalContractAddress = '0x8fa83dcabe18d6C1bDfe31a31bC5B5e73eF34E62';
-export const RinkebyContractAddress = '0x55431c6282be3260041a98dE94203Eb60EccaF57';
+export const RinkebyContractAddress = '0x516b363f3B115D858F7a71370Fe56F64ae8C466F';
 export const LocalContractABI = abi.abi; 
 export const RinkebyContractABI = rinkebyAbi.abi;
